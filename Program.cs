7c13170165ef195a99d83c4366fa9a21110a8b1e
Renan Martins <renanmartins1999@hotmail.com>
2022-12-20 22:45:00 -0300

    Inicializando repositorio

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.VisualBasic.FileIO;
+using OfficeOpenXml;
+
+Console.WriteLine($"Match Words");
+
+var filePath = FileSystem.GetFileInfo("Simulacao_tabelas_do_banco.xlsx");
+var existingFile = new FileInfo(filePath.ToString());
+
+using(var package = new ExcelPackage(existingFile)){
+        
+}
+
+
+class Tabela {
+    public List<string>? Colunas;
+}
+
+
